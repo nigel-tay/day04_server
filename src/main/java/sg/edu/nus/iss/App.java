82053d297bbae22a92c3385377e7dd5761f3ae0a
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-04-20 14:44:37 +0800

    Update Cookie.java

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,13 +1,81 @@
 package sg.edu.nus.iss;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        String filePathName = args[0];
+        String portNumber = args[1];
+
+        File cookieFile = new File(filePathName);
+
+        if (!cookieFile.exists()) {
+            System.out.println("Cookie file not found boohoo :( Guhbye!!");
+            System.exit(0);
+        }
+
+        // Slide 8
+        ServerSocket ss = new ServerSocket(Integer.parseInt(portNumber));
+        Socket socket = ss.accept();
+
+        // Store the data sent over from client, e.g. get-cookie
+        String msgReceived = "";
+
+        // Slide 9
+        try (InputStream is = socket.getInputStream()) {
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis);
+
+            try (OutputStream os = socket.getOutputStream()) {
+                BufferedOutputStream bos = new BufferedOutputStream(os);
+                DataOutputStream dos = new DataOutputStream(bos);
+
+                // Write logic to receive and send. Putting the while loop here allows us to close only after everything is done. No reapeated opening and closing
+                while (!msgReceived.equals("close")) {
+                    // slide 9 - receive message
+                    msgReceived = dis.readUTF();
+
+                    if (msgReceived.equals("get-cookie")) {
+                        // instantiate cookie.java
+                        // Get random cookie 
+                        // Send it back to client using DataOutputStream (dos.writeUTF(xxxxxxxx))
+                    }
+                }
+                // close all output stream
+                dos.close();
+                bos.close();
+                os.close();
+            }
+            catch (EOFException e) {
+                e.printStackTrace();
+                socket.close();
+            }
+
+            // close all input stream in reverse order
+            dis.close();
+            bis.close();
+            is.close();
+        }
+        catch (EOFException e) {
+            e.printStackTrace();
+            socket.close();
+            ss.close();
+        }
     }
 }
---
+++ src/main/java/sg/edu/nus/iss/Cookie.java
@@ -0,0 +1,40 @@
+package sg.edu.nus.iss;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cookie {
+    List <String> cookies = null;
+
+    public void readCookieFile(String fileName) throws FileNotFoundException, IOException {
+        // Instantiate cookies collection object
+        cookies = new ArrayList<>();
+
+        // Use a File object to pass the fileName
+        File newFile = new File(fileName);
+
+        // Use FileReader and BufferedReader for reading from cookie file
+        // Instantiate FileReader and BufferedReader
+        FileReader fr = new FileReader(newFile);
+        BufferedReader br = new BufferedReader(fr);
+        String currentLine = "";
+        // While loop to go through the file
+        // Read each line and add into the cookies collection object
+        while((currentLine = br.readLine()) != null) {
+            cookies.add(currentLine);
+        }
+
+        // Close fr and br
+        br.close();
+        fr.close();
+    }
+
+    public String getRandomCookie() {
+        return "";
+    }
+}
